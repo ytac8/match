5f8b463bfb6e0761fbfc38daa327f88b58920211
ytac8 <y.tac0223@gmail.com>
2018-05-11 17:45:04 +0900

    first commit

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main-page">
+        <Header></Header>
+        <MainContent></MainContent>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+// import Footer from '../components/Footer.vue'
+// import MainContent from '../components/MainContent'
+
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      hello: 'hello'
+    }
+  },
+  components: {
+    Header
+    // Footer,
+    // MainContent
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
